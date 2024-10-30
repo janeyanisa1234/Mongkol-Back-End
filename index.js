76b3eb066cce7e9c740eed2ab3b6fdec5c63ca51
VachirapornBerkban <Vachiraphon.b@ku.th>
2024-10-30 04:05:47 +0000

    first try

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const bodyParser = require ('body-parser');
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin","*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  );
+  res.setHeader(
+   /*"Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE", */
+  );
+  next();
+});
+app.use(express.json())
+//ตัวแปรที่เก็บ Products, วันที่?
+
+/* 
+  var con = mysql.createConnection({
+  host: "korawit.ddns.net",
+  user: "webapp",
+  password: "secret",
+  port: "3307",
+  database: "shop",
+}); 
+*/
+
+con.connect(function(err){
+  if(err) throw err;
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/api/products',(req,res)=>{
+
 })
 
 app.listen(port, () => {
